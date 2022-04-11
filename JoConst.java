a53308aeb2ca33d92cc9fca95bfa2300d3bdbe48
Joshgiku17 <87287313+Joshgiku17@users.noreply.github.com>
2022-04-11 06:02:33 +0200

    Create JoConst.java

---
+++ JoConst.java
@@ -0,0 +1,9 @@
+class JoConst{
+    public JoConst(){
+        System.out.println("Let's try a constructor...")
+    }
+    public JoConst(int a, int b){
+        int answer= a*b;
+        
+    }
+}
